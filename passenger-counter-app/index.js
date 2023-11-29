0cb054ce125e869a27ed16e0c45d1e29b1ee9479
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-11-30 00:12:29 +0300

    Practices added

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,5 +1,5 @@
-//define the count element
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el"); //define the count element
+let saveEl = document.getElementById("save-el");
 console.log(countEl);
 
 //initalize the count as 0
@@ -8,11 +8,21 @@ console.log(count);
 
 //Listen for clicks on the increment button
 function increment() {
-  console.log("The button was clicked");
+  console.log("The button was clicked"); //increment the count variable when the button is clicked
+  count += 1; //change the count-el in the HTML to reflect the new count
+  countEl.textContent = count;
+}
 
-  //increment the count variable when the button is clicked
-  count = count + 1;
+//create a function that saves the count
+function save() {
+  console.log(count + " saved");
 
-  //change the count-el in the HTML to reflect the new count
-  countEl.innerText = count;
+  let countStr = count + " - ";
+  //Create a variable that contains both the count and the dash operator e.x. "12 -"
+
+  saveEl.textContent += countStr; //Render the variable in the saveEl using inner text
+  //NB? Make sure not to delete the existing content of the paragraph
+
+  count = 0;
+  countEl.textContent = "0";
 }
---
+++ practices/index.js
@@ -0,0 +1,37 @@
+let firstName = "Durukan";
+
+let lastName = "Kizilarslan";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//Function to greet with output
+let name1 = "Linda";
+let greeting = "Hi there";
+
+function hiGreet() {
+  let greetingFull = greeting + ", " + name1 + "!";
+  console.log(greetingFull);
+}
+
+hiGreet();
+
+//-----------------------------
+
+let myPoints = 3;
+
+function add3Points() {
+  myPoints += 3;
+}
+
+function remove1Point() {
+  myPoints -= 1;
+}
+
+add3Points();
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+console.log(myPoints);
