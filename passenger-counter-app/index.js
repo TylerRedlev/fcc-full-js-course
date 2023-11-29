8d2fea1138370dbecd01266ef75761e7d941a6d3
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-11-29 18:28:03 +0300

    Passenger counter functionality added

--- passenger-counter-app/index.js
+++ passenger-counter-app/index.js
@@ -1,10 +1,18 @@
-// document.getElementById("count-el").innerText = 5;
+//define the count element
+let countEl = document.getElementById("count-el");
+console.log(countEl);
 
+//initalize the count as 0
 let count = 0;
 console.log(count);
 
-//myAge variable
-let myAge = 30;
-console.log(myAge);
+//Listen for clicks on the increment button
+function increment() {
+  console.log("The button was clicked");
 
-//test comment
+  //increment the count variable when the button is clicked
+  count = count + 1;
+
+  //change the count-el in the HTML to reflect the new count
+  countEl.innerText = count;
+}
