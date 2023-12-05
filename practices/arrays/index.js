e660d81dd930c49d6f6d96671d533c84244bfc4d
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-12-05 19:27:38 +0300

    function return practiced

---
+++ practices/arrays/index.js
@@ -0,0 +1,10 @@
+let player1Time = 102;
+let player2Time = 107;
+
+//Write a function that returns the total race time
+function getTotalTime() {
+  return player1Time + player2Time;
+}
+
+let totalTime = getTotalTime();
+console.log(totalTime);
