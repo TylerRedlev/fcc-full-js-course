63089199c0758a876ef21fed765e345577339d27
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-12-06 00:51:18 +0300

    random number added

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,23 +1,53 @@
 console.log("Blackjack");
 
-let firstCard = 9;
-let secondCard = 9;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+}
+
 function startGame() {
+  isAlive = true;
+
+  let firstCard = getRandomNumber();
+  let secondCard = getRandomNumber();
+  cards = [firstCard, secondCard];
+
   renderGame();
 }
 
 function renderGame() {
+  cardsEl.textContent = "Cards: ";
+
+  //Render cards in the current state
+  for (i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i];
+
+    // Check if the current element is the last one in the array
+    if (i === cards.length - 1) {
+      console.log("This is the last element.");
+    } else {
+      cardsEl.textContent += " - ";
+    }
+  }
+
   sumEl.textContent += " " + sum;
-  cardsEl.textContent += " " + cards[0] + " - " + cards[1];
+  //cardsEl.textContent += " " + cards[0] + " - " + cards[1];
   if (sum < 21) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -33,11 +63,12 @@ function renderGame() {
 
 function newCard() {
   //1. Create a card variable, and hard code its value to a number (2-11)
-  let card = 8;
+  let card = getRandomNumber();
   console.log(card + "added");
   //Add the new card to the sum variable
   sum += card;
-
+  cards.push(card);
+  console.log(cards);
   //Call start game function
   renderGame();
 }
