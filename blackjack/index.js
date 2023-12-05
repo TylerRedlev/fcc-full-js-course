9389959f57e0307616acc1429befd3ef974320e5
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-12-05 15:18:56 +0300

    Cards array added

--- blackjack/index.js
+++ blackjack/index.js
@@ -2,6 +2,7 @@ console.log("Blackjack");
 
 let firstCard = 9;
 let secondCard = 9;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -11,8 +12,12 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
   sumEl.textContent += " " + sum;
-  cardsEl.textContent += " " + firstCard + " - " + secondCard;
+  cardsEl.textContent += " " + cards[0] + " - " + cards[1];
   if (sum < 21) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -24,4 +29,15 @@ function startGame() {
   }
   messageEl.textContent = message;
   console.log(isAlive);
+}
+
+function newCard() {
+  //1. Create a card variable, and hard code its value to a number (2-11)
+  let card = 8;
+  console.log(card + "added");
+  //Add the new card to the sum variable
+  sum += card;
+
+  //Call start game function
+  renderGame();
 }
