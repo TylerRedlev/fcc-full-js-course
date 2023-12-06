ef5e959a8c650c51286a67af75abc0969781c827
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-12-06 15:29:13 +0300

    Object player updated

--- blackjack/index.js
+++ blackjack/index.js
@@ -5,9 +5,17 @@ let sum = 0;
 let hasBlackJack = false;
 let isAlive = false;
 
+let player = {
+  name: "Per",
+  chips: 145,
+};
+
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomNumber() {
   let randomNumber = Math.floor(Math.random() * 13) + 1;
