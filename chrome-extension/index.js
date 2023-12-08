5c1a1c63d56e6de932096c92e899652773e292ec
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-12-08 12:37:47 +0300

    Render leads function created

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -8,17 +8,27 @@ const ulEl = document.getElementById("list-el");
 
 btnSave.addEventListener("click", function () {
   leads.push(inputEl.value);
+  inputEl.value = "";
 
+  renderLeads();
+});
+
+function renderLeads() {
   ulEl.innerHTML = "";
+  let listItems = "";
 
   for (i = 0; i < leads.length; i++) {
-    //ulEl.innerHTML += "<li>" + leads[i] + "</li>";
+    //listItems += "<li><a target='_blank' href='#'>" + leads[i] + "</a></li>";
+    listItems += `<li><a target='_blank' href='#'> ${leads[i]} </a></li>`;
+    //Create listItems for list items HTML
 
     //create li const
     //add element of leads to the list item
     //add to ul through append
-    const li = document.createElement("li");
-    li.textContent = leads[i];
-    ulEl.append(li);
+    // const li = document.createElement("li");
+    // li.textContent = leads[i];
+    // listItems.textContent += li;
   }
-});
+
+  ulEl.innerHTML = listItems;
+}
